8ab8dd4a0a7976c2d2365cb175330a16c8f5b3a6
unknown <calm1025@gmail.com>
2022-04-04 06:17:44 +0900

    rm test

--- youtube-mobile/main.js
+++
@@ -1,9 +0,0 @@
-const moreBtn = document.querySelector(".info .metadata .moreBtn")
-const title = document.querySelector(".info .metadata .title")
-
-console.log(moreBtn)
-
-moreBtn.addEventListener("click", () => {
-  moreBtn.classList.toggle("clicked")
-  title.classList.toggle("clamp")
-})
